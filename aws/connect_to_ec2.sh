4c6b1893b6705ce1d43833aa0a73ae14d0e09f3c
Yannik <pitcany@gmail.com>
2020-09-28 12:15:07 -0700

    now I can use vnc viewer

--- aws/connect_to_ec2.sh
+++ aws/connect_to_ec2.sh
@@ -3,4 +3,4 @@
 ip_address=$(aws ec2 describe-instances --instance-ids $instance_id --output text --query 'Reservations[*].Instances[*].PublicIpAddress')
 echo ip_address=$ip_address
 
-ssh -L 8888:localhost:8888 yannik@$ip_address
+ssh -L 8888:localhost:8888 -L 5902:localhost:5902 yannik@$ip_address
